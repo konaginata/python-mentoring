6814bb10f98c7c12fdcf3e1dc3c437947580ac2c
Nataliia Vrzheshch <n.vrzheshch@eis-partners.com>
2023-08-30 10:12:00 +0200

    Homework 2: Task 4 reworked precisely to the draft

--- hw2/task4_find_util.py
+++ hw2/task4_find_util.py
@@ -1,28 +1,50 @@
+#!/usr/bin/env python
+import sys
+import argparse
 import os
 import fnmatch
-import argparse
 
 
-def find_util(root_dir, pattern="*", file_type="all"):
-    items_found = []
-    for root, dirs, files in os.walk(root_dir):
-        for name in files + dirs:
-            if fnmatch.fnmatch(name, pattern):
-                if file_type == "d" and not os.path.isdir(os.path.join(root, name)):
-                    continue
-                elif file_type == "f" and not os.path.isfile(os.path.join(root, name)):
-                    continue
-                items_found.append(os.path.join(root, name))
-        return items_found
+def find(folder, name=None, show_dirs=True, show_files=True):
+    """
+    :param folder: path to a system folder from where to start searching
+    :param name: file/directory name pattern, allows using '*' and '?' symbols
+    :param show_dirs: if True - include directories into search results
+    :param show_files: if True - include files into search results
+    """
+    pattern = '*' if name is None else name
+    for root, dirs, files in os.walk(folder):
+        if show_files:
+            for filename in fnmatch.filter(files, pattern):
+                print(os.path.join(root, filename))
+        if show_dirs:
+            for dirname in fnmatch.filter(dirs, pattern):
+                print(os.path.join(root, dirname))
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Search tool for files and directories")
-    parser.add_argument("root_dir", help="root directory for search", type=str)
-    parser.add_argument("-name", help="item name pattern", default="*")
-    parser.add_argument("-type", help="item type: f (file) || d (directory) || all (both) as default",
-                        choices=["f", "d", "all"], default="all")
-    args = parser.parse_args()
-    items = find_util(args.root_dir, args.name, args.type)
-    for item in items:
-        print(item)
+def parse_cmd_args():
+    path_help = "Path to a folder"
+    pattern_help = "File/directory name pattern. Allows using '*' and '?' symbols"
+    type_help = "Where 'f' means search only files, 'd' means only directories"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help=path_help)
+    parser.add_argument('-name', nargs='?', default=None, help=pattern_help)
+    parser.add_argument('-type', nargs='?', default=None, choices=['f', 'd'], help=type_help)
+
+    if len(sys.argv) <= 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    cmd, _ = parser.parse_known_args()
+
+    files, dirs = True, True
+    if cmd.type == 'd':
+        files = False
+    if cmd.type == 'f':
+        dirs = False
+    return cmd.path, cmd.name, dirs, files
+
+
+if __name__ == '__main__':
+    args = parse_cmd_args()
+    find(*args)
