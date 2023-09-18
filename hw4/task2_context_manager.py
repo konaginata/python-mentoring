b4848c9ddfee4b68d6429d75ec42bdcd2624e035
Nataliia Vrzheshch <n.vrzheshch@eis-partners.com>
2023-09-18 12:27:06 +0200

    Homework 4: Task 2 implemented

---
+++ hw4/task2_context_manager.py
@@ -0,0 +1,20 @@
+from time import time, sleep
+
+
+class timer:
+    def __init__(self, block_name):
+        self.block_name = block_name
+
+    def __enter__(self):
+        self.start_time = time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        elapsed_time = time() - self.start_time
+        print(f"timer: block '{self.block_name}' executed in {elapsed_time:.3f} sec")
+
+
+with timer('doing some sleeps'):
+    sleep(1)
+    sleep(2)
+    sleep(3)
